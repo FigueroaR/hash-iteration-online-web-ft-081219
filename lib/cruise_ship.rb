26986ed2a4863edac0c129b04804c560461f6130
Raul Figueroa <raul.a.figueroa@hotmail.com>
2019-08-19 07:56:50 +0000

    Automatically backed up by Learn

--- lib/cruise_ship.rb
+++ lib/cruise_ship.rb
@@ -1,7 +1,8 @@
-
+require 'pry'
 def select_winner(passengers)
   passengers.each do |suite, name|
   if suite == :suite_a && name.start_with?("A")
+    binding.pry
     winner = name
   end
   puts winner
